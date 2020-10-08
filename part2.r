9e7c6095ce43c6ae55dda9496159b8f50c17a0b6
tovehenning <tove.a.henning@gmail.com>
2020-10-08 13:12:00 +0200

    Update part2.r

--- part2.r
+++ part2.r
@@ -1,10 +1,26 @@
 library(StatProg)
-library(ggplot2)
+library(tidyverse)
 
+data("galaxies")
+x = as.numeric(galaxies)
 
-galaxies <- as.data.frame(galaxies)
-names(galaxies) <- "km"
+gammaUpdate = function(x, mu, sigma, pi){
+  pdf = t(sapply(x, dnorm, mean = mu, sd = sigma))
+  for(n in 1:length(x)){
+    for(k in 1:length(mu)){
+      pdf[1, 2] = pi[k]*pdf[n, k]
+    }
+  }
+  gam = as.data.frame(matrix(NA, ncol = length(pi), nrow = length(x)))
+  for(n in 1:length(x)){
+    for(k in 1:length(mu)){
+      gam[n, k] = pdf[n, k]/sum(pdf[n,])
+    }
+  }
+  return(as.data.frame(gam))
+}
 
-ggplot(galaxies, aes(x = km)) +
-    geom_density()
-
+mu = c(10000, 20000, 32000)
+sigma = c(1000, 2000, 3000)
+pi = c(1/3, 1/3, 1/3)
+test = gammaUpdate(x, mu, sigma, pi)
