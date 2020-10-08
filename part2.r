cb87e29c3e9d99ad1599afd5a6d3c0a7497cc499
tovehenning <71635556+tovehenning@users.noreply.github.com>
2020-10-08 13:35:17 +0200

    Update part2.r

--- part2.r
+++ part2.r
@@ -26,3 +26,19 @@ gammaUpdate = function(x, mu, sigma, pi){
   return(as.data.frame(gam))
 }
 
+mu = c(10, 20, 30)
+sigma = c(2, 2, 2)
+probs = c(1/3, 1/3, 1/3)
+
+resp = gammaUpdate(galaxies, mu, sigma, probs)
+mu = muUpdate(galaxies, resp)
+sigma = sigmaUpdate(galaxies, resp, mu)
+probs = piUpdate(resp)
+
+cat("mu:", mu,
+"\nsigma:", sigma,
+"\nprobs:", probs,
+"\nresp[1,]", resp[1,])
+
+
+
